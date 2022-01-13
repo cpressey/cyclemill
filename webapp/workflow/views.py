f4192d7ceff9386d7a48709b13ecea9125a73e13
Chris Pressey <cpressey@catseye.tc>
2022-01-13 21:32:03 +0000

    Workflows view shows workflows.

--- webapp/workflow/views.py
+++ webapp/workflow/views.py
@@ -2,19 +2,20 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.views.decorators.http import require_POST
 from django.shortcuts import render
 
-from django_celery_results.models import TaskResult
+#from django_celery_results.models import TaskResult
 
+from workflow.models import Workflow, WorkflowTask
 from workflow.tasks import start_basic_workflow
 
 
 def home(request):
     return render(request, 'home.html', {
-        'task_results': TaskResult.objects.all().order_by('date_done')
+        'workflows': Workflow.objects.all().order_by('started_at')
     })
 
 
 @require_POST
 def launch(request):
-    start_basic_workflow.delay(30)
+    start_basic_workflow(30)
     # django.messages
     return HttpResponseRedirect('/')
