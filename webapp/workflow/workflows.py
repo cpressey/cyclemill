15f29453d6c5b1348b50c1fef9d3950eb2446280
Chris Pressey <cpressey@catseye.tc>
2022-01-24 15:35:20 +0000

    Add docstring for the planned functionality here.

--- webapp/workflow/workflows.py
+++ webapp/workflow/workflows.py
@@ -9,8 +9,13 @@ logger = logging.getLogger(__name__)
 
 
 def start_workflow_canvas(signature):
+    """The supplied `signature` should be the signature of a task that takes a
+    workflow ID as its first argument (all other arguments to the task must be
+    included in the supplied signature), and returns that same workflow ID
+    as its result."""
     now = timezone.now()
     workflow = Workflow.objects.create(type='Basic', started_at=now)
     logger.info('[Workflow {}] is now {}'.format(workflow.id, workflow.status))
+    # TODO: signature.link(workflow_final_task.s())
     signature.delay(workflow.id)
     return workflow
